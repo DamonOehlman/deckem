4d6b566966c7898d9d284833233a85be85d634d8
Damon Oehlman <damon.oehlman@gmail.com>
2011-09-13 21:54:56 +1000

    Dogfooding deckem

--- src/deckem.js
+++ src/deckem.js
@@ -1,6 +1,8 @@
 //= http://code.jquery.com/jquery-1.6.2.js
 
 //= deck.js!core/deck.core
+//= deck.js!extensions/hash/deck.hash.js
+//= deck.js!extensions/status/deck.status.js
 //= keymaster!
 //= deckmirror!deck.codemirror.js
 
