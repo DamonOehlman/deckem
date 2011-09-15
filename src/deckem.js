e8919d0bbae56a7d53e2e9a04f77d4a92180aaba
Damon Oehlman <damon.oehlman@gmail.com>
2011-09-15 14:16:26 +1000

    Updated to include deck.scale

--- src/deckem.js
+++ src/deckem.js
@@ -3,7 +3,7 @@
 //= deck.js!core/deck.core
 //= deck.js!extensions/hash/deck.hash.js
 //= deck.js!extensions/status/deck.status.js
-//= keymaster!
+//= deck.js!extensions/scale/deck.scale.js
 //= deckmirror!deck.codemirror.js
 
 //= deck.js!modernizr.custom.js
